5146de0a64fcd8706d4a904cf9053ce478a84b02
Anton <stells@ngs.ru>
2021-05-01 22:37:56 +0300

    edited scopes 4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import KeyvMongoDB from "keyv-mongodb";
 
 dotenv.config();
-import {InstallProvider, LogLevel} from "@slack/oauth";
+import { InstallProvider, LogLevel } from "@slack/oauth";
 import express from "express";
 import Keyv from "keyv";
 import mongoose from "mongoose";
@@ -10,83 +10,88 @@ import mongoose from "mongoose";
 const app = express();
 const PORT = 3000;
 mongoose.connect(process.env.DB_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 const keyv = new Keyv({
-    store: new KeyvMongoDB({mongooseConnection: mongoose.connection}),
+  store: new KeyvMongoDB({ mongooseConnection: mongoose.connection }),
 });
 keyv.on("error", (err) => console.log("Connection Error", err));
 
-
 const installer = new InstallProvider({
-    clientId: process.env.SLACK_CLIENT_ID,
-    clientSecret: process.env.SLACK_CLIENT_SECRET,
-    authVersion: "v2",
-    stateSecret: "my-state-secret",
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  authVersion: "v2",
+  stateSecret: "my-state-secret",
 
-    logLevel: LogLevel.DEBUG,
-    installationStore: {
-        storeInstallation: async (installation) => {
-            if (installation.isEnterpriseInstall) {
-                // storing org installation
-                return await keyv.set(installation.enterprise.id, installation);
-            } else if (
-                installation.team !== null &&
-                installation.team.id !== undefined
-            ) {
-                // storing single team installation
-                return await keyv.set(installation.team.id, installation);
-            }
-            throw new Error("Failed saving installation data to installationStore");
-        },
-        fetchInstallation: async (installQuery) => {
-            if (installQuery.isEnterpriseInstall) {
-                if (installQuery.enterpriseId !== undefined) {
-                    // fetching org installation
-                    return await keyv.get(installQuery.enterpriseId);
-                }
-            }
-            if (installQuery.teamId !== undefined) {
-                // fetching single team installation
-                return await keyv.get(installQuery.teamId);
-            }
-            throw new Error("Failed fetching installation");
-        },
+  logLevel: LogLevel.DEBUG,
+  installationStore: {
+    storeInstallation: async (installation) => {
+      if (installation.isEnterpriseInstall) {
+        // storing org installation
+        return await keyv.set(installation.enterprise.id, installation);
+      } else if (
+        installation.team !== null &&
+        installation.team.id !== undefined
+      ) {
+        // storing single team installation
+        return await keyv.set(installation.team.id, installation);
+      }
+      throw new Error("Failed saving installation data to installationStore");
     },
+    fetchInstallation: async (installQuery) => {
+      if (installQuery.isEnterpriseInstall) {
+        if (installQuery.enterpriseId !== undefined) {
+          // fetching org installation
+          return await keyv.get(installQuery.enterpriseId);
+        }
+      }
+      if (installQuery.teamId !== undefined) {
+        // fetching single team installation
+        return await keyv.get(installQuery.teamId);
+      }
+      throw new Error("Failed fetching installation");
+    },
+  },
 });
 
 app.get("/", (req, res) => res.send("go to /slack/install"));
 app.get("/slack/install", async (req, res, next) => {
-    try {
-        // feel free to modify the scopes
-        const url = await installer.generateInstallUrl({
-            scopes: ["bot", "commands", "im:history", "incoming-webhook", "channels:history", "channels:read"],
-        });
-        res.send(
-            `<a href=${url}><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>`
-        );
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    // feel free to modify the scopes
+    const url = await installer.generateInstallUrl({
+      scopes: [
+        "commands",
+        "im:history",
+        "incoming-webhook",
+        "channels:history",
+        "channels:read",
+      ],
+    });
+    res.send(
+      `<a href=${url}><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>`
+    );
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // using custom success and failure handlers
 const callbackOptions = {
-    success: (installation, installOptions, req, res) => {
-        res.send("successful!");
-    },
-    failure: (error, installOptions, req, res) => {
-        res.send("failure");
-    },
+  success: (installation, installOptions, req, res) => {
+    res.send("successful!");
+  },
+  failure: (error, installOptions, req, res) => {
+    res.send("failure");
+  },
 };
 
 app.get("/slack/oauth_redirect", async (req, res) => {
-    await installer.handleCallback(req, res, callbackOptions);
+  await installer.handleCallback(req, res, callbackOptions);
 });
 
 app.listen(process.env.PORT || PORT, () =>
-    console.log(
-        `Example app listening on port ${PORT}! Go to https://redirect-server-url.herokuapp.com/slack/install to initiate oauth flow`
-    )
+  console.log(
+    `Example app listening on port ${PORT}! Go to https://redirect-server-url.herokuapp.com/slack/install to initiate oauth flow`
+  )
 );
