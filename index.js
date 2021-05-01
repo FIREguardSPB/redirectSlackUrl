a31839a72cd72a9ef07907a2556c1e5835ebcaec
Anton <stells@ngs.ru>
2021-05-01 15:38:07 +0300

    edited scopes

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.get("/slack/install", async (req, res, next) => {
     try {
         // feel free to modify the scopes
         const url = await installer.generateInstallUrl({
-            scopes: ["commands", "im:history", "incoming-webhook", "channels:history", "channels:read", "users:read.email"],
+            scopes: ["commands", "im:history", "incoming-webhook", "channels:history"],
         });
         res.send(
             `<a href=${url}><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>`
