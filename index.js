b8c300262ae9e2ba77671fb73232279e856a1159
Anton <stells@ngs.ru>
2021-05-05 13:08:02 +0300

    refactoring before release

--- index.js
+++ index.js
@@ -1,105 +1,3 @@
-// import dotenv from "dotenv";
-// import KeyvMongoDB from "keyv-mongodb";
-//
-// dotenv.config();
-// import { InstallProvider, LogLevel } from "@slack/oauth";
-// import express from "express";
-// import Keyv from "keyv";
-// import mongoose from "mongoose";
-//
-// const app = express();
-// const PORT = 3000;
-// mongoose.connect(process.env.DB_URL, {
-//   useUnifiedTopology: true,
-//   useNewUrlParser: true,
-// });
-// const keyv = new Keyv({
-//   store: new KeyvMongoDB({ mongooseConnection: mongoose.connection }),
-// });
-// keyv.on("error", (err) => console.log("Connection Error", err));
-//
-// const installer = new InstallProvider({
-//   clientId: process.env.SLACK_CLIENT_ID,
-//   clientSecret: process.env.SLACK_CLIENT_SECRET,
-//   authVersion: "v2",
-//   stateSecret: "my-state-secret",
-//
-//   logLevel: LogLevel.DEBUG,
-//   installationStore: {
-//     storeInstallation: async (installation) => {
-//       if (installation.isEnterpriseInstall) {
-//         // storing org installation
-//         return await keyv.set(installation.enterprise.id, installation);
-//       } else if (
-//         installation.team !== null &&
-//         installation.team.id !== undefined
-//       ) {
-//         // storing single team installation
-//         return await keyv.set(installation.team.id, installation);
-//       }
-//       throw new Error("Failed saving installation data to installationStore");
-//     },
-//     fetchInstallation: async (installQuery) => {
-//       if (installQuery.isEnterpriseInstall) {
-//         if (installQuery.enterpriseId !== undefined) {
-//           // fetching org installation
-//           return await keyv.get(installQuery.enterpriseId);
-//         }
-//       }
-//       if (installQuery.teamId !== undefined) {
-//         // fetching single team installation
-//         console.log(await keyv.get(installQuery.teamId));
-//         return await keyv.get(installQuery.teamId);
-//       }
-//       throw new Error("Failed fetching installation");
-//     },
-//   },
-// });
-//
-// app.get("/", (req, res) => res.send("go to /slack/install"));
-// app.get("/slack/install", async (req, res, next) => {
-//   try {
-//     // feel free to modify the scopes
-//     const url = await installer.generateInstallUrl({
-//       scopes: [
-//         "commands",
-//         "im:history",
-//         // "incoming-webhook",
-//         "channels:history",
-//         "channels:read",
-//         "chat:write",
-//       ],
-//     });
-//     res.send(
-//       `<a href=${url}><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>`
-//     );
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-//
-// // using custom success and failure handlers
-// const callbackOptions = {
-//   success: (installation, installOptions, req, res) => {
-//     res.send("successful!");
-//   },
-//   failure: (error, installOptions, req, res) => {
-//     res.send("failure");
-//   },
-// };
-//
-// app.get("/slack/oauth_redirect", async (req, res) => {
-//   await installer.handleCallback(req, res, callbackOptions);
-// });
-//
-// app.listen(process.env.PORT || PORT, () =>
-//   console.log(
-//     `Example app listening on port ${PORT}! Go to https://redirect-server-url.herokuapp.com/slack/install to initiate oauth flow`
-//   )
-// );
-
-//============================================================================================================
-
 import dotenv from "dotenv";
 import KeyvMongoDB from "keyv-mongodb";
 
@@ -107,16 +5,10 @@ dotenv.config();
 import pkg from "@slack/bolt";
 
 const { App } = pkg;
-import { InstallProvider, LogLevel } from "@slack/oauth";
-
 import Keyv from "keyv";
 import mongoose from "mongoose";
 
 const PORT = 3000;
-// mongoose.connect(process.env.DB_URL, {
-//   useUnifiedTopology: true,
-//   useNewUrlParser: true,
-// });
 const keyv = new Keyv({
   store: new KeyvMongoDB({ mongooseConnection: mongoose.connection }),
 });
@@ -128,12 +20,6 @@ const app = new App({
   clientSecret: process.env.SLACK_CLIENT_SECRET,
   stateSecret: "my-state-secret",
   scopes: [
-    // "channels:read",
-    // "groups:read",
-    // "channels:manage",
-    // "chat:write",
-    // "incoming-webhook",
-    //
     "channels:history",
     "channels:manage",
     "channels:read",
@@ -182,10 +68,3 @@ const app = new App({
   await app.start(process.env.PORT || PORT);
   console.log(`⚡️ Bolt app is running! port ${PORT}`);
 })();
-
-//
-// botToken?: string;
-// userToken?: string;
-// botId?: string;
-// botUserId?: string;
-// teamId?: string;
